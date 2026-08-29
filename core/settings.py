445befc989bdc86fca0aa8469042f0e76210b1b5
Awio Albert Awio <albertawioawio1@gmail.com>
2026-08-29 05:38:55 +0300

    Complete Phase 4: Django architecture, routes, views, and templates

--- core/settings.py
+++ core/settings.py
@@ -64,3 +64,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to Green Valley School - Website setup is complete!")
+    return render(request, 'index.html')
